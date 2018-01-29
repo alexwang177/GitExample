cd039e455ff79c9a525d7e3b3862ab4f2b30767f
Alex Wang <alexwang177@gmail.com>
2018-01-29 13:24:46 -0600

    add map

---
+++ GitExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitExample
+//
+//  Created by AlexWang1 on 1/29/18.
+//  Copyright © 2018 Alex Wang. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
