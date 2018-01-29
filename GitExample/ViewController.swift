e8fe79f7d39474ae48309dbe31e8ab5ac257c890
Alex Wang <alexwang177@gmail.com>
2018-01-29 13:16:44 -0600

    Added a button.

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        func buttonClicked(_sender: UIButton){
+            print("This is a git tutorial")
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
